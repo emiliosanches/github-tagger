5ef68d9c9af7d0a43a9bc68a8293a160de5de378
Emilio Sanches <emiliosneto13@gmail.com>
2024-03-15 16:22:53 -0300

    [web/feat]: Create web app with skeleton of login page

---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+// import { Container } from './styles';
+
+export function Login() {
+  return (
+    <div>
+      <h1>Bem-vindo!</h1>
+      <p>Entre no nosso sistema utilizando o GitHub</p>
+      <button>Entrar com GitHub</button>
+    </div>
+  );
+}
