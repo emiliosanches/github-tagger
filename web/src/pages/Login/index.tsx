318e42949a7ec1568fdb35cf1a0e45f15f1fd1eb
Emilio Sanches <emiliosneto13@gmail.com>
2024-03-16 17:03:13 -0300

    [web/feat]: Add link to github login in login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,11 +1,15 @@
 // import { Container } from './styles';
 
+const githubLoginURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${
+  import.meta.env.VITE_CLIENT_ID
+}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_REDIRECT_URI)}`;
+
 export function Login() {
   return (
     <div>
       <h1>Bem-vindo!</h1>
       <p>Entre no nosso sistema utilizando o GitHub</p>
-      <button>Entrar com GitHub</button>
+      <a href={githubLoginURL}>Entrar com GitHub</a>
     </div>
   );
 }
